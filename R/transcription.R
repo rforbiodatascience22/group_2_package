ca18e401c84d7858934fc4775cddca488102c266
Yuann123 <s212420@dtu.dk>
2022-03-23 12:35:26 +0000

    insert the condon_table

--- R/transcription.R
+++ R/transcription.R
@@ -7,6 +7,6 @@
 #'
 #' @examples
 transcrition <- function(codons){
-  protein <- paste0(name_of_your_object[codons], collapse = "")
+  protein <- paste0(codon_table[codons], collapse = "")
   return(protein)
 }
